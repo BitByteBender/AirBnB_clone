00e19d98f5d4e381d2450715ebe551238c715953
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-03-09 11:18:37 -0800

    updating file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,17 +36,21 @@ class FileStorage:
         """
         deserializes the JSON file to __objects
         (only if the JSON file (_file_path) exists
-        Otherwise nothing
+        Otherwise nothing.
+
         If file doesn't exist throw an exception
         """
         try:
             from models.base_model import BaseModel
+            from models.user import User
             with open(self.__file_path, mode='r', encoding="utf-8") as file:
                 serializeObjs = json.load(file)
                 for k, val in serializeObjs.items():
                     cls_name, objId = k.split('.')
-                    objDict = val
-                    obj = eval(cls_name)(**objDict)
+                    if cls_name == 'User':
+                        obj = User(**val)
+                    else:
+                        obj = BaseModel(**val)
                     self.__objects[k] = obj
         except FileNotFoundError:
             pass
