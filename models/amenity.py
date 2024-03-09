1691da1f8567c71e3fdc3a167394cf558e634bf4
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-03-09 12:01:58 -0800

    Added more classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+""" Creating: Amenity class """
+from base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Amenity class core that inherits from BaseModel """
+    name = ""
--- models/review.py
+++ models/review.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" Creating: Review class """
+from base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Review core class that inherits from BaseModel """
+    place_id = ""
+    user_id = ""
+    text = ""
