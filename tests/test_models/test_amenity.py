daf2c17a062c4b3c10c3ffcdf5bd56158e00282d
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-03-10 12:03:17 -0700

    Updated amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -7,19 +7,23 @@ from models.base_model import BaseModel
 
 class TestAmenity(unittest.TestCase):
     def testing_attrs(self):
+        """ Testing if Amenity class has an attr called "name" """
         amt = Amenity()
         self.assertTrue(hasattr(amt, "name"))
 
     def testing_attr_vals(self):
+        """ Testing dflt value of name attr """
         amt = Amenity()
         self.assertEqual(amt.name, "")
 
     def testing_attrs_params(self):
+        """ Testing name attribute """
         amenity = Amenity()
         amenity.name = "WiFi"
         self.assertEqual(amenity.name, "WiFi")
 
     def testing_to_dict_method(self):
+        """ Testing the to_dict method"""
         amt = Amenity()
         amt.name = "AC"
         amtDict = amt.to_dict()
@@ -28,6 +32,7 @@ class TestAmenity(unittest.TestCase):
         self.assertEqual(amtDict["name"], "AC")
 
     def testing_BM_inheritance(self):
+        """ Testing inheritance from BaseModel"""
         self.assertTrue(issubclass(Amenity, BaseModel))
 
 
